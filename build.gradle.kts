e8567ebcd9c116d11c7ae14fa32d56ab67a3b922
Xiaomckedou233 <xiaomckedou233@xiaomcserver.top>
2024-07-09 18:09:03 +0800

    1.20

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.author.plugin"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    val paperVersion: String by project
+    compileOnly("io.papermc.paper:paper-api:$paperVersion-R0.1-SNAPSHOT")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
---
+++ src/main/kotlin/com/author/plugin/ExamplePlugin.kt
@@ -0,0 +1,13 @@
+package com.author.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExamplePlugin : JavaPlugin() {
+    override fun onEnable() {
+        TODO()
+    }
+
+    override fun onDisable() {
+        TODO()
+    }
+}
